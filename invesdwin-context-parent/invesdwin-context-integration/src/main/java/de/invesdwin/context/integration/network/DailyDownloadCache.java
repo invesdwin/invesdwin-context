f36bd850fd27ffa28c87d8863cad3d413ae5ab89
subes <gsubes@gmail.com>
2016-08-17 18:50:34 +0200

    DailyDownloadCache

---
+++ invesdwin-context-parent/invesdwin-context-integration/src/main/java/de/invesdwin/context/integration/network/DailyDownloadCache.java
@@ -0,0 +1,52 @@
+package de.invesdwin.context.integration.network;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.Callable;
+
+import javax.annotation.concurrent.NotThreadSafe;
+
+import org.apache.commons.io.FileUtils;
+
+import de.invesdwin.context.ContextProperties;
+import de.invesdwin.util.time.duration.Duration;
+import de.invesdwin.util.time.fdate.FDate;
+import de.invesdwin.util.time.fdate.FTimeUnit;
+
+@NotThreadSafe
+public class DailyDownloadCache {
+
+    private static final Duration DAILY_REFRESH_DURATION = new Duration(1, FTimeUnit.DAYS);
+    private static final File FOLDER = new File(ContextProperties.getHomeDirectory(),
+            DailyDownloadCache.class.getSimpleName());
+
+    public String download(final String name, final Callable<String> request) throws Exception {
+        try {
+            final File file = newFile(name);
+            if (!file.exists()
+                    || !ContextProperties.IS_TEST_ENVIRONMENT && shouldUpdate(FDate.valueOf(file.lastModified()))) {
+                final String content = request.call();
+                FileUtils.writeStringToFile(file, content);
+                return content;
+            } else {
+                return FileUtils.readFileToString(file);
+            }
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static File newFile(final String name) {
+        return new File(FOLDER, name + ".txt");
+    }
+
+    public static void delete(final String name) {
+        final File file = newFile(name);
+        FileUtils.deleteQuietly(file);
+    }
+
+    public boolean shouldUpdate(final FDate lastRequestTime) {
+        return new Duration(lastRequestTime).isGreaterThan(DAILY_REFRESH_DURATION)
+                || !FDate.isSameDay(lastRequestTime, new FDate());
+    }
+}
