d8171ad60d3d37ce9afc62320bd1939da090e34f
subes <gsubes@gmail.com>
2017-09-28 17:35:00 +0200

    speed up xpath

--- invesdwin-context-parent/invesdwin-context/src/main/java/de/invesdwin/context/beans/init/platform/util/internal/XmlTransformerConfigurer.java
+++ invesdwin-context-parent/invesdwin-context/src/main/java/de/invesdwin/context/beans/init/platform/util/internal/XmlTransformerConfigurer.java
@@ -2,6 +2,8 @@ package de.invesdwin.context.beans.init.platform.util.internal;
 
 import javax.annotation.concurrent.Immutable;
 
+import de.invesdwin.util.lang.Reflections;
+
 @Immutable
 public final class XmlTransformerConfigurer {
 
@@ -10,8 +12,15 @@ public final class XmlTransformerConfigurer {
     public static void configure() {
         //as long as xalan does not support jaxp 1.4 the sun impl must be used
         //CHECKSTYLE:OFF do not use SystemProperties class to not cause static intializers from firing too early
-        System.setProperty("javax.xml.transform.TransformerFactory",
-                "com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl");
+        final String transformerImpl = "com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl";
+        if (Reflections.classExists(transformerImpl)) {
+            System.setProperty("javax.xml.transform.TransformerFactory", transformerImpl);
+        }
+
+        //https://blog.jooq.org/2013/08/25/how-to-speed-up-apache-xalans-xpath-processor-by-factor-10x/
+        System.setProperty("org.apache.xml.dtm.DTMManager", "org.apache.xml.dtm.ref.DTMManagerDefault");
+        System.setProperty("com.sun.org.apache.xml.internal.dtm.DTMManager",
+                "com.sun.org.apache.xml.internal.dtm.ref.DTMManagerDefault");
         //CHECKSTYLE:ON
     }
 
