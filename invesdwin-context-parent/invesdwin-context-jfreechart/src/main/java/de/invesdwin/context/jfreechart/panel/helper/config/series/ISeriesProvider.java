9b55202950e0fa6344d0f79ecb5831214668b60c
subes <gsubes@gmail.com>
2019-04-05 17:34:06 +0400

    make renderertype private

--- invesdwin-context-parent/invesdwin-context-jfreechart/src/main/java/de/invesdwin/context/jfreechart/panel/helper/config/series/ISeriesProvider.java
+++ invesdwin-context-parent/invesdwin-context-jfreechart/src/main/java/de/invesdwin/context/jfreechart/panel/helper/config/series/ISeriesProvider.java
@@ -1,7 +1,6 @@
 package de.invesdwin.context.jfreechart.panel.helper.config.series;
 
 import de.invesdwin.context.jfreechart.panel.InteractiveChartPanel;
-import de.invesdwin.context.jfreechart.panel.helper.config.SeriesRendererType;
 import de.invesdwin.util.math.expression.IExpression;
 
 public interface ISeriesProvider {
@@ -13,8 +12,6 @@ public interface ISeriesProvider {
     String getExpressionName();
 
     String getExpressionString(IExpression[] args);
-
-    SeriesRendererType getRendererType();
 
     ISeriesParameter[] getParameters();
 
