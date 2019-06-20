2c845d2998f853c0864492b18d23bbec92277b29
subes <gsubes@gmail.com>
2019-06-20 16:40:09 +0100

    don't use jni in lz4

--- invesdwin-context-parent/invesdwin-context-integration/src/main/java/de/invesdwin/context/integration/streams/LZ4Streams.java
+++ invesdwin-context-parent/invesdwin-context-integration/src/main/java/de/invesdwin/context/integration/streams/LZ4Streams.java
@@ -61,8 +61,9 @@ public final class LZ4Streams {
 
     public static LZ4BlockOutputStream newHighLZ4OutputStream(final OutputStream out, final int blockSize,
             final int compressionLevel) {
-        return new LZ4BlockOutputStream(out, blockSize, LZ4Factory.fastestInstance().highCompressor(compressionLevel),
-                XXHashFactory.fastestInstance().newStreamingHash32(DEFAULT_SEED).asChecksum(), true);
+        return new LZ4BlockOutputStream(out, blockSize,
+                LZ4Factory.fastestJavaInstance().highCompressor(compressionLevel),
+                XXHashFactory.fastestJavaInstance().newStreamingHash32(DEFAULT_SEED).asChecksum(), true);
     }
 
     public static LZ4BlockOutputStream newLargeFastLZ4OutputStream(final OutputStream out) {
@@ -74,12 +75,12 @@ public final class LZ4Streams {
     }
 
     public static LZ4BlockOutputStream newFastLZ4OutputStream(final OutputStream out, final int blockSize) {
-        return new LZ4BlockOutputStream(out, blockSize, LZ4Factory.fastestInstance().fastCompressor(),
-                XXHashFactory.fastestInstance().newStreamingHash32(DEFAULT_SEED).asChecksum(), true);
+        return new LZ4BlockOutputStream(out, blockSize, LZ4Factory.fastestJavaInstance().fastCompressor(),
+                XXHashFactory.fastestJavaInstance().newStreamingHash32(DEFAULT_SEED).asChecksum(), true);
     }
 
     public static LZ4BlockInputStream newDefaultLZ4InputStream(final InputStream in) {
-        return new LZ4BlockInputStream(in, LZ4Factory.fastestInstance().fastDecompressor());
+        return new LZ4BlockInputStream(in, LZ4Factory.fastestJavaInstance().fastDecompressor());
     }
 
 }
