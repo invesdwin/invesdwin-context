44f574c53ec8a537062e8998eccc65431616a2e5
subes <gsubes@gmail.com>
2019-06-26 18:34:51 +0200

    not bootstrap test

--- tests/otherproject-noparent-bom-test/src/main/java/com/otherproject/scripting/ScriptingWithoutBootstrapMain.java
+++ tests/otherproject-noparent-bom-test/src/main/java/com/otherproject/scripting/ScriptingWithoutBootstrapMain.java
@@ -10,95 +10,110 @@ import de.invesdwin.context.r.runtime.contract.AScriptTaskR;
 import de.invesdwin.util.assertions.Assertions;
 
 public class ScriptingWithoutBootstrapMain {
-	
+
 	public static void main(String[] args) {
-		//disable invesdwin bootstrap to make this more lightweight
+		// disable invesdwin bootstrap to make this more lightweight
 		PlatformInitializerProperties.setAllowed(false);
-		
+
+		/*
+		 * alternatively only disable e.g. logback configuration so that you can use
+		 * your existing logger configuration
+		 * 
+		 * if you want to use log4j2 instead of logback, just exclude the maven
+		 * artifact, include the slf4j redirect for logback and add the dependency for
+		 * slf4j-log4j2
+		 */
+//		PlatformInitializerProperties.setInitializer(new DelegatePlatformInitializer(PlatformInitializerProperties.getInitializer()) {
+//			@Override
+//			public void initLogbackConfigurationLoader() {
+//				//noop to disable this bootstrap step
+//			}
+//		});
+
 		callScriptPython();
 		callScriptR();
 		callScriptMatlab();
-		
-		//need to explicitly call system.exit so that threads are stopped properly
+
+		// need to explicitly call system.exit so that threads are stopped properly
 		System.exit(0);
 	}
 
 	private static void callScriptPython() {
 		final AScriptTaskPython<String> script = new AScriptTaskPython<String>() {
 
-		    @Override
-		    public void populateInputs(final IScriptTaskInputs inputs) {
-			inputs.putString("hello", "World");
-		    }
+			@Override
+			public void populateInputs(final IScriptTaskInputs inputs) {
+				inputs.putString("hello", "World");
+			}
 
-		    @Override
-		    public void executeScript(final IScriptTaskEngine engine) {
-			//execute this script inline:
-			engine.eval("world = \"Hello \" + hello + \"!\"");
-			//or run it from a file:
-			//engine.eval(new ClassPathResource("HelloWorldScript.py", getClass()));
-		    }
+			@Override
+			public void executeScript(final IScriptTaskEngine engine) {
+				// execute this script inline:
+				engine.eval("world = \"Hello \" + hello + \"!\"");
+				// or run it from a file:
+				// engine.eval(new ClassPathResource("HelloWorldScript.py", getClass()));
+			}
 
-		    @Override
-		    public String extractResults(final IScriptTaskResults results) {
-			return results.getString("world");
-		    }
+			@Override
+			public String extractResults(final IScriptTaskResults results) {
+				return results.getString("world");
+			}
 		};
-		final String result = script.run(); //optionally pass a specific runner as an argument here
+		final String result = script.run(); // optionally pass a specific runner as an argument here
 		Assertions.assertThat(result).isEqualTo("Hello World!");
-		System.out.println("Python: "+result);
+		System.out.println("Python: " + result);
 	}
 
 	private static void callScriptMatlab() {
 		final AScriptTaskMatlab<String> script = new AScriptTaskMatlab<String>() {
 
-		    @Override
-		    public void populateInputs(final IScriptTaskInputs inputs) {
-			inputs.putString("hello", "World");
-		    }
+			@Override
+			public void populateInputs(final IScriptTaskInputs inputs) {
+				inputs.putString("hello", "World");
+			}
 
-		    @Override
-		    public void executeScript(final IScriptTaskEngine engine) {
-			//execute this script inline:
-			engine.eval("world = strcat({'Hello '}, hello, '!')");
-			//or run it from a file:
-			//engine.eval(new ClassPathResource("HelloWorldScript.m", getClass()));
-		    }
+			@Override
+			public void executeScript(final IScriptTaskEngine engine) {
+				// execute this script inline:
+				engine.eval("world = strcat({'Hello '}, hello, '!')");
+				// or run it from a file:
+				// engine.eval(new ClassPathResource("HelloWorldScript.m", getClass()));
+			}
 
-		    @Override
-		    public String extractResults(final IScriptTaskResults results) {
-		        return results.getString("world");
-		    }
+			@Override
+			public String extractResults(final IScriptTaskResults results) {
+				return results.getString("world");
+			}
 		};
-		final String result = script.run(); //optionally pass a specific runner as an argument here
+		final String result = script.run(); // optionally pass a specific runner as an argument here
 		Assertions.assertThat(result).isEqualTo("Hello World!");
-		System.out.println("Matlab: "+result);
+		System.out.println("Matlab: " + result);
 	}
 
 	private static void callScriptR() {
 		final AScriptTaskR<String> script = new AScriptTaskR<String>() {
 
-		    @Override
-		    public void populateInputs(final IScriptTaskInputs inputs) {
-			inputs.putString("hello", "World");
-		    }
+			@Override
+			public void populateInputs(final IScriptTaskInputs inputs) {
+				inputs.putString("hello", "World");
+			}
 
-		    @Override
-		    public void executeScript(final IScriptTaskEngine engine) {
-			//execute this script inline:
-			engine.eval("world <- paste(\"Hello \", hello, \"!\", sep=\"\")");
-			//or run it from a file:
-			//engine.eval(new ClassPathResource("HelloWorldScript.R", getClass()));
-		    }
+			@Override
+			public void executeScript(final IScriptTaskEngine engine) {
+				// execute this script inline:
+				engine.eval("world <- paste(\"Hello \", hello, \"!\", sep=\"\")");
+				// or run it from a file:
+				// engine.eval(new ClassPathResource("HelloWorldScript.R", getClass()));
+			}
 
-		    @Override
-		    public String extractResults(final IScriptTaskResults results) {
-		        return results.getString("world");
-		    }
+			@Override
+			public String extractResults(final IScriptTaskResults results) {
+				return results.getString("world");
+			}
 		};
-		final String result = script.run(); //optionally pass a specific runner as an argument here
+		final String result = script.run(); // optionally pass a specific runner as an argument here
 		Assertions.assertThat(result).isEqualTo("Hello World!");
-		System.out.println("R: "+result);
+		System.out.println("R: " + result);
 	}
 
 }
