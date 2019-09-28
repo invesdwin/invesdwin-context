5194298da1cf26a745e890379fb5ef2b670c63ab
subes <gsubes@gmail.com>
2019-09-28 16:38:30 +0200

    write with count

--- invesdwin-context-parent/invesdwin-context-integration/src/main/java/de/invesdwin/context/integration/csv/IBeanWriter.java
+++ invesdwin-context-parent/invesdwin-context-integration/src/main/java/de/invesdwin/context/integration/csv/IBeanWriter.java
@@ -2,24 +2,33 @@ package de.invesdwin.context.integration.csv;
 
 import java.io.Closeable;
 import java.io.IOException;
+import java.util.NoSuchElementException;
 
 import de.invesdwin.util.collections.iterable.ICloseableIterable;
 import de.invesdwin.util.collections.iterable.ICloseableIterator;
 
 public interface IBeanWriter<E> extends Closeable {
 
-    default void write(final ICloseableIterable<? extends E> iterable) throws IOException {
+    default int write(final ICloseableIterable<? extends E> iterable) throws IOException {
+        int count = 0;
         try (ICloseableIterator<? extends E> iterator = iterable.iterator()) {
-            while (iterator.hasNext()) {
+            while (true) {
                 write(iterator.next());
+                count++;
             }
+        } catch (final NoSuchElementException e) {
+            //end reached
         }
+        return count;
     }
 
-    default void write(final Iterable<? extends E> iterable) throws IOException {
+    default int write(final Iterable<? extends E> iterable) throws IOException {
+        int count = 0;
         for (final E e : iterable) {
             write(e);
+            count++;
         }
+        return count;
     }
 
     void write(E e) throws IOException;
