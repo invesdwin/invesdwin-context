d7238af875f5263b393a6a6b30519e2524dbb7e1
subes <gsubes@gmail.com>
2020-01-30 19:11:57 +0100

    fix close

--- invesdwin-context-parent/invesdwin-context-jasperreports/src/main/java/de/invesdwin/context/jasperreports/internal/ConfiguredSubreportRunnerFactory.java
+++ invesdwin-context-parent/invesdwin-context-jasperreports/src/main/java/de/invesdwin/context/jasperreports/internal/ConfiguredSubreportRunnerFactory.java
@@ -1,9 +1,13 @@
 package de.invesdwin.context.jasperreports.internal;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 
 import javax.annotation.concurrent.Immutable;
 
+import de.invesdwin.util.concurrent.ADelegateExecutorService;
 import de.invesdwin.util.concurrent.Executors;
 import de.invesdwin.util.concurrent.WrappedExecutorService;
 import net.sf.jasperreports.engine.fill.JRFillContext;
@@ -19,7 +23,33 @@ public class ConfiguredSubreportRunnerFactory extends ThreadPoolSubreportRunnerF
 
     @Override
     protected ExecutorService createThreadExecutor(final JRFillContext fillContext) {
-        return EXECUTOR;
+        return new ADelegateExecutorService(EXECUTOR) {
+
+            @Override
+            protected Runnable newRunnable(final Runnable runnable) {
+                return runnable;
+            }
+
+            @Override
+            protected <T> Callable<T> newCallable(final Callable<T> callable) {
+                return callable;
+            }
+
+            @Override
+            public void shutdown() {
+                try {
+                    EXECUTOR.awaitPendingCount(0);
+                } catch (final InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            @Override
+            public List<Runnable> shutdownNow() {
+                //noop
+                return Collections.emptyList();
+            }
+        };
     }
 
 }
