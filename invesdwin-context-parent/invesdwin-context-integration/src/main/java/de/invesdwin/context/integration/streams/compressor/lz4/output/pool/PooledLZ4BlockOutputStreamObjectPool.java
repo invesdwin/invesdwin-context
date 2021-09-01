bd33767a9c44f4e11b6c8c13db4b91e4fd47d40f
subes <gsubes@gmail.com>
2021-09-02 01:32:26 +0200

    list is too slow

--- invesdwin-context-parent/invesdwin-context-integration/src/main/java/de/invesdwin/context/integration/streams/compressor/lz4/output/pool/PooledLZ4BlockOutputStreamObjectPool.java
+++ invesdwin-context-parent/invesdwin-context-integration/src/main/java/de/invesdwin/context/integration/streams/compressor/lz4/output/pool/PooledLZ4BlockOutputStreamObjectPool.java
@@ -2,11 +2,11 @@ package de.invesdwin.context.integration.streams.compressor.lz4.output.pool;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import java.util.zip.Checksum;
 
 import javax.annotation.concurrent.NotThreadSafe;
 
+import de.invesdwin.util.collections.iterable.buffer.BufferingIterator;
 import de.invesdwin.util.concurrent.pool.AObjectPool;
 import net.jpountz.lz4.LZ4Compressor;
 
@@ -14,7 +14,7 @@ import net.jpountz.lz4.LZ4Compressor;
 public final class PooledLZ4BlockOutputStreamObjectPool extends AObjectPool<PooledLZ4BlockOutputStream> {
 
     private final int maxPoolSize;
-    private final List<PooledLZ4BlockOutputStream> pooledLZ4BlockOutputStreamRotation = new ArrayList<PooledLZ4BlockOutputStream>();
+    private final BufferingIterator<PooledLZ4BlockOutputStream> pooledLZ4BlockOutputStreamRotation = new BufferingIterator<PooledLZ4BlockOutputStream>();
 
     public PooledLZ4BlockOutputStreamObjectPool(final int blockSize, final LZ4Compressor compressor,
             final Checksum checksum, final int maxPoolSize) {
@@ -28,7 +28,7 @@ public final class PooledLZ4BlockOutputStreamObjectPool extends AObjectPool<Pool
         if (pooledLZ4BlockOutputStreamRotation.isEmpty()) {
             return factory.makeObject();
         }
-        final PooledLZ4BlockOutputStream pooledLZ4BlockOutputStream = pooledLZ4BlockOutputStreamRotation.remove(0);
+        final PooledLZ4BlockOutputStream pooledLZ4BlockOutputStream = pooledLZ4BlockOutputStreamRotation.next();
         if (pooledLZ4BlockOutputStream != null) {
             return pooledLZ4BlockOutputStream;
         } else {
@@ -45,7 +45,7 @@ public final class PooledLZ4BlockOutputStreamObjectPool extends AObjectPool<Pool
     public synchronized Collection<PooledLZ4BlockOutputStream> internalClear() {
         final Collection<PooledLZ4BlockOutputStream> removed = new ArrayList<PooledLZ4BlockOutputStream>();
         while (!pooledLZ4BlockOutputStreamRotation.isEmpty()) {
-            removed.add(pooledLZ4BlockOutputStreamRotation.remove(0));
+            removed.add(pooledLZ4BlockOutputStreamRotation.next());
         }
         return removed;
     }
@@ -71,7 +71,7 @@ public final class PooledLZ4BlockOutputStreamObjectPool extends AObjectPool<Pool
 
     @Override
     protected synchronized void internalRemoveObject(final PooledLZ4BlockOutputStream obj) {
-        pooledLZ4BlockOutputStreamRotation.remove(obj);
+        pooledLZ4BlockOutputStreamRotation.next();
     }
 
 }
