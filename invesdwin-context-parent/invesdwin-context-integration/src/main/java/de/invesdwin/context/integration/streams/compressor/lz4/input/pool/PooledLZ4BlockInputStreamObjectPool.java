ea9cf78d13fa66772af81648656505abe213d60d
subes <gsubes@gmail.com>
2021-09-02 13:05:34 +0200

    some pools with better implementations

--- invesdwin-context-parent/invesdwin-context-integration/src/main/java/de/invesdwin/context/integration/streams/compressor/lz4/input/pool/PooledLZ4BlockInputStreamObjectPool.java
+++ invesdwin-context-parent/invesdwin-context-integration/src/main/java/de/invesdwin/context/integration/streams/compressor/lz4/input/pool/PooledLZ4BlockInputStreamObjectPool.java
@@ -1,80 +1,30 @@
 package de.invesdwin.context.integration.streams.compressor.lz4.input.pool;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Queue;
 import java.util.zip.Checksum;
 
-import javax.annotation.concurrent.NotThreadSafe;
+import javax.annotation.concurrent.ThreadSafe;
 
 import org.agrona.concurrent.ManyToManyConcurrentArrayQueue;
 
-import de.invesdwin.util.concurrent.pool.commons.ACommonsObjectPool;
+import de.invesdwin.util.concurrent.pool.AQueueObjectPool;
 import net.jpountz.lz4.LZ4FastDecompressor;
 
-@NotThreadSafe
-public final class PooledLZ4BlockInputStreamObjectPool extends ACommonsObjectPool<PooledLZ4BlockInputStream> {
+@ThreadSafe
+public final class PooledLZ4BlockInputStreamObjectPool extends AQueueObjectPool<PooledLZ4BlockInputStream> {
 
-    private final int maxPoolSize;
-    private final Queue<PooledLZ4BlockInputStream> pooledLZ4BlockInputStreamRotation;
+    private final LZ4FastDecompressor decompressor;
+    private final Checksum checksum;
 
     public PooledLZ4BlockInputStreamObjectPool(final LZ4FastDecompressor decompressor, final Checksum checksum,
             final int maxPoolSize) {
-        super(null);
-        setFactory(new PooledLZ4BlockInputStreamObjectFactory(this, decompressor, checksum));
-        this.maxPoolSize = maxPoolSize;
-        pooledLZ4BlockInputStreamRotation = new ManyToManyConcurrentArrayQueue<PooledLZ4BlockInputStream>(maxPoolSize);
+        super(new ManyToManyConcurrentArrayQueue<PooledLZ4BlockInputStream>(maxPoolSize));
+        this.decompressor = decompressor;
+        this.checksum = checksum;
     }
 
     @Override
-    protected PooledLZ4BlockInputStream internalBorrowObject() {
-        if (pooledLZ4BlockInputStreamRotation.isEmpty()) {
-            return factory.makeObject();
-        }
-        final PooledLZ4BlockInputStream poledLZ4BlockInputStream = pooledLZ4BlockInputStreamRotation.poll();
-        if (poledLZ4BlockInputStream != null) {
-            return poledLZ4BlockInputStream;
-        } else {
-            return factory.makeObject();
-        }
-    }
-
-    @Override
-    public int getNumIdle() {
-        return pooledLZ4BlockInputStreamRotation.size();
-    }
-
-    @Override
-    public Collection<PooledLZ4BlockInputStream> internalClear() {
-        final Collection<PooledLZ4BlockInputStream> removed = new ArrayList<PooledLZ4BlockInputStream>();
-        while (!pooledLZ4BlockInputStreamRotation.isEmpty()) {
-            removed.add(pooledLZ4BlockInputStreamRotation.poll());
-        }
-        return removed;
-    }
-
-    @Override
-    protected PooledLZ4BlockInputStream internalAddObject() {
-        final PooledLZ4BlockInputStream pooled = factory.makeObject();
-        pooledLZ4BlockInputStreamRotation.offer(factory.makeObject());
-        return pooled;
-    }
-
-    @Override
-    protected void internalReturnObject(final PooledLZ4BlockInputStream obj) {
-        if (pooledLZ4BlockInputStreamRotation.size() < maxPoolSize) {
-            pooledLZ4BlockInputStreamRotation.offer(obj);
-        }
-    }
-
-    @Override
-    protected void internalInvalidateObject(final PooledLZ4BlockInputStream obj) {
-        //Nothing happens
-    }
-
-    @Override
-    protected void internalRemoveObject(final PooledLZ4BlockInputStream obj) {
-        pooledLZ4BlockInputStreamRotation.poll();
+    protected PooledLZ4BlockInputStream newObject() {
+        return new PooledLZ4BlockInputStream(decompressor, checksum, this);
     }
 
 }
--- invesdwin-context-parent/invesdwin-context-integration/src/main/java/de/invesdwin/context/integration/streams/compressor/lz4/output/pool/PooledLZ4BlockOutputStreamObjectPool.java
+++ invesdwin-context-parent/invesdwin-context-integration/src/main/java/de/invesdwin/context/integration/streams/compressor/lz4/output/pool/PooledLZ4BlockOutputStreamObjectPool.java
@@ -1,81 +1,32 @@
 package de.invesdwin.context.integration.streams.compressor.lz4.output.pool;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Queue;
 import java.util.zip.Checksum;
 
-import javax.annotation.concurrent.NotThreadSafe;
+import javax.annotation.concurrent.ThreadSafe;
 
 import org.agrona.concurrent.ManyToManyConcurrentArrayQueue;
 
-import de.invesdwin.util.concurrent.pool.commons.ACommonsObjectPool;
+import de.invesdwin.util.concurrent.pool.AQueueObjectPool;
 import net.jpountz.lz4.LZ4Compressor;
 
-@NotThreadSafe
-public final class PooledLZ4BlockOutputStreamObjectPool extends ACommonsObjectPool<PooledLZ4BlockOutputStream> {
+@ThreadSafe
+public final class PooledLZ4BlockOutputStreamObjectPool extends AQueueObjectPool<PooledLZ4BlockOutputStream> {
 
-    private final int maxPoolSize;
-    private final Queue<PooledLZ4BlockOutputStream> pooledLZ4BlockOutputStreamRotation;
+    private final int blockSize;
+    private final LZ4Compressor compressor;
+    private final Checksum checksum;
 
     public PooledLZ4BlockOutputStreamObjectPool(final int blockSize, final LZ4Compressor compressor,
             final Checksum checksum, final int maxPoolSize) {
-        super(null);
-        setFactory(new PooledLZ4BlockOutputStreamObjectFactory(this, blockSize, compressor, checksum));
-        this.maxPoolSize = maxPoolSize;
-        pooledLZ4BlockOutputStreamRotation = new ManyToManyConcurrentArrayQueue<PooledLZ4BlockOutputStream>(
-                maxPoolSize);
+        super(new ManyToManyConcurrentArrayQueue<PooledLZ4BlockOutputStream>(maxPoolSize));
+        this.blockSize = blockSize;
+        this.compressor = compressor;
+        this.checksum = checksum;
     }
 
     @Override
-    protected PooledLZ4BlockOutputStream internalBorrowObject() {
-        if (pooledLZ4BlockOutputStreamRotation.isEmpty()) {
-            return factory.makeObject();
-        }
-        final PooledLZ4BlockOutputStream pooledLZ4BlockOutputStream = pooledLZ4BlockOutputStreamRotation.poll();
-        if (pooledLZ4BlockOutputStream != null) {
-            return pooledLZ4BlockOutputStream;
-        } else {
-            return factory.makeObject();
-        }
-    }
-
-    @Override
-    public int getNumIdle() {
-        return pooledLZ4BlockOutputStreamRotation.size();
-    }
-
-    @Override
-    public Collection<PooledLZ4BlockOutputStream> internalClear() {
-        final Collection<PooledLZ4BlockOutputStream> removed = new ArrayList<PooledLZ4BlockOutputStream>();
-        while (!pooledLZ4BlockOutputStreamRotation.isEmpty()) {
-            removed.add(pooledLZ4BlockOutputStreamRotation.poll());
-        }
-        return removed;
-    }
-
-    @Override
-    protected PooledLZ4BlockOutputStream internalAddObject() {
-        final PooledLZ4BlockOutputStream pooled = factory.makeObject();
-        pooledLZ4BlockOutputStreamRotation.offer(factory.makeObject());
-        return pooled;
-    }
-
-    @Override
-    protected void internalReturnObject(final PooledLZ4BlockOutputStream obj) {
-        if (pooledLZ4BlockOutputStreamRotation.size() < maxPoolSize) {
-            pooledLZ4BlockOutputStreamRotation.offer(obj);
-        }
-    }
-
-    @Override
-    protected void internalInvalidateObject(final PooledLZ4BlockOutputStream obj) {
-        //Nothing happens
-    }
-
-    @Override
-    protected void internalRemoveObject(final PooledLZ4BlockOutputStream obj) {
-        pooledLZ4BlockOutputStreamRotation.poll();
+    protected PooledLZ4BlockOutputStream newObject() {
+        return new PooledLZ4BlockOutputStream(blockSize, compressor, checksum, this);
     }
 
 }
