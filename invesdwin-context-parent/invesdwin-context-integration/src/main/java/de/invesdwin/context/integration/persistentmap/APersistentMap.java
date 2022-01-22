b36d857ff5986292f36460b7c3fca4124f2b2929
Edwin Stang <gsubes@gmail.com>
2022-01-22 18:37:44 +0100

    better reason

--- invesdwin-context-parent/invesdwin-context-integration/src/main/java/de/invesdwin/context/integration/persistentmap/APersistentMap.java
+++ invesdwin-context-parent/invesdwin-context-integration/src/main/java/de/invesdwin/context/integration/persistentmap/APersistentMap.java
@@ -204,7 +204,8 @@ public abstract class APersistentMap<K, V> extends APersistentMapConfig<K, V> im
         readLock.lock();
         if (tableFinalizer.table == null) {
             readLock.unlock();
-            throw new RetryLaterRuntimeException("table should not be null here");
+            throw new RetryLaterRuntimeException(
+                    "table might have been deleted in the mean time, thus retry initialization");
         }
         return tableFinalizer.table;
     }
