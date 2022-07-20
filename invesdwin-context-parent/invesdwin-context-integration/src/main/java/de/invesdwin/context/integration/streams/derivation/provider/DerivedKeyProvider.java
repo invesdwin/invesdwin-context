ef914db255aa2565aa61a27f6d3935047dcd8138
Edwin Stang <gsubes@gmail.com>
2022-07-21 00:35:20 +0200

    comment on the salt

--- invesdwin-context-parent/invesdwin-context-integration/src/main/java/de/invesdwin/context/integration/streams/derivation/provider/DerivedKeyProvider.java
+++ invesdwin-context-parent/invesdwin-context-integration/src/main/java/de/invesdwin/context/integration/streams/derivation/provider/DerivedKeyProvider.java
@@ -5,15 +5,34 @@ import javax.annotation.concurrent.Immutable;
 import de.invesdwin.context.integration.streams.derivation.IDerivationFactory;
 import de.invesdwin.context.integration.streams.derivation.hkdf.HkdfDerivationFactory;
 
+/**
+ * Key derivation techniques are: Password+PBKDF2+HKDFexpands or Random+HKDFextract+HKDFexpands
+ * 
+ * We can derive AES-KEY, AES-IV, MAC-KEY from the initial Password or Random. scrypt and bcrypt are alternatives to
+ * PBKDF2
+ * 
+ * The salt should be a random value always that can be publicly exchanged before deriving additional passwords. The
+ * password or random value should be transmitted over a secure channel or pre-shared. Though if a secure channel is
+ * used anyhow, then it is best to transmit the salt and the key together over the secure channel. Using a null or fixed
+ * salt is less secure.
+ * 
+ * https://security.stackexchange.com/questions/48000/why-would-you-need-a-salt-for-aes-cbs-when-iv-is-already-randomly-generated-and
+ */
 @Immutable
 public class DerivedKeyProvider implements IDerivedKeyProvider {
 
     private final IDerivationFactory derivationFactory;
+    private final byte[] salt;
     private final byte[] key;
 
-    public DerivedKeyProvider(final IDerivationFactory derivationFactory, final byte[] key) {
-        this.derivationFactory = derivationFactory;
+    public DerivedKeyProvider(final byte[] salt, final byte[] key, final IDerivationFactory derivationFactory) {
+        this.salt = salt;
         this.key = key;
+        this.derivationFactory = derivationFactory;
+    }
+
+    public byte[] getSalt() {
+        return salt;
     }
 
     @Override
@@ -47,7 +66,7 @@ public class DerivedKeyProvider implements IDerivedKeyProvider {
     public static DerivedKeyProvider fromRandom(final byte[] salt, final byte[] random,
             final IDerivationFactory derivationFactory) {
         final byte[] key = derivationFactory.extract(salt, random);
-        return new DerivedKeyProvider(derivationFactory, key);
+        return new DerivedKeyProvider(salt, key, derivationFactory);
 
     }
 
