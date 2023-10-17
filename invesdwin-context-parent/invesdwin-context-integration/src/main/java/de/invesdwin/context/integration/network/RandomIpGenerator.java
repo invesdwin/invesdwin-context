a97cd9694a622f8bca318177eb6240b96f3d348f
Edwin Stang <gsubes@gmail.com>
2023-10-18 00:43:49 +0200

    maxExclusive

--- invesdwin-context-parent/invesdwin-context-integration/src/main/java/de/invesdwin/context/integration/network/RandomIpGenerator.java
+++ invesdwin-context-parent/invesdwin-context-integration/src/main/java/de/invesdwin/context/integration/network/RandomIpGenerator.java
@@ -16,8 +16,7 @@ import de.invesdwin.util.math.random.PseudoRandomGenerators;
 @Immutable
 public final class RandomIpGenerator {
 
-    private RandomIpGenerator() {
-    }
+    private RandomIpGenerator() {}
 
     /**
      * Returns a random ip without private classes.
@@ -41,22 +40,22 @@ public final class RandomIpGenerator {
     }
 
     private static int getRandomByte() {
-        return PseudoRandomGenerators.getThreadLocalPseudoRandom().nextInt(0, 255); //Generates from 0 to 255
+        return PseudoRandomGenerators.getThreadLocalPseudoRandom().nextInt(0, 256); //Generates from 0 to 255
     }
 
-    private static boolean isPrivateClassA(final int eins) {
-        return eins == 10 || eins == 0 || eins == 127;
+    private static boolean isPrivateClassA(final int one) {
+        return one == 10 || one == 0 || one == 127;
     }
 
-    private static boolean isPrivateClassB(final int eins, final int zwei) {
-        return eins == 172 && zwei >= 16 && zwei <= 31;
+    private static boolean isPrivateClassB(final int one, final int two) {
+        return one == 172 && two >= 16 && two <= 31;
     }
 
-    private static boolean isPrivateClassC(final int eins, final int zwei) {
-        return eins == 192 && zwei == 168 || eins == 169 && zwei == 254;
+    private static boolean isPrivateClassC(final int one, final int two) {
+        return one == 192 && two == 168 || one == 169 && two == 254;
     }
 
-    private static boolean isBroadcast(final int eins, final int zwei, final int drei, final int vier) {
-        return eins == 255 && zwei == 255 && drei == 255 && vier == 255;
+    private static boolean isBroadcast(final int one, final int two, final int three, final int four) {
+        return one == 255 && two == 255 && three == 255 && four == 255;
     }
 }
