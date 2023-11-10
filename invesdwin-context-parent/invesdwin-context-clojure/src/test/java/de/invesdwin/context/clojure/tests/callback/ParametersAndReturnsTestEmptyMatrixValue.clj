e6d6f3a251a25f4149c05fe7feddb630b0f7e24f
Edwin Stang <gsubes@gmail.com>
2023-11-10 19:59:13 +0100

    some progress with clojure callback

--- invesdwin-context-parent/invesdwin-context-clojure/src/test/java/de/invesdwin/context/clojure/tests/callback/ParametersAndReturnsTestEmptyMatrixValue.clj
+++ invesdwin-context-parent/invesdwin-context-clojure/src/test/java/de/invesdwin/context/clojure/tests/callback/ParametersAndReturnsTestEmptyMatrixValue.clj
@@ -1,119 +1,141 @@
-(def getBooleanMatrix putBooleanMatrix)
+(def getBooleanMatrix (callback "getBooleanMatrix"))
 (if-not (= (count getBooleanMatrix) 2)
 	(throw (Exception. "getBooleanMatrix empty!"))
 )
+(callback "set" get)
 
-(def getBooleanMatrixAsList putBooleanMatrixAsList)
+(def getBooleanMatrixAsList (callback "getBooleanMatrixAsList"))
 (if-not (= (count getBooleanMatrixAsList) 2)
 	(throw (Exception. "getBooleanMatrixAsList empty!"))
 )
+(callback "set" get)
 
 
-(def getByteMatrix putByteMatrix)
+(def getByteMatrix (callback "getByteMatrix"))
 (if-not (= (count getByteMatrix) 2)
 	(throw (Exception. "getByteMatrix empty!"))
 )
+(callback "set" get)
 
-(def getByteMatrixAsList putByteMatrixAsList)
+(def getByteMatrixAsList (callback "getByteMatrixAsList"))
 (if-not (= (count getByteMatrixAsList) 2)
 	(throw (Exception. "getByteMatrixAsList empty!"))
 )
+(callback "set" get)
 
 
-(def getCharacterMatrix putCharacterMatrix)
+(def getCharacterMatrix (callback "getCharacterMatrix"))
 (if-not (= (count getCharacterMatrix) 2)
 	(throw (Exception. "getCharacterMatrix empty!"))
 )
+(callback "set" get)
 
-(def getCharacterMatrixAsList putCharacterMatrixAsList)
+(def getCharacterMatrixAsList (callback "getCharacterMatrixAsList"))
 (if-not (= (count getCharacterMatrixAsList) 2)
 	(throw (Exception. "getCharacterMatrixAsList empty!"))
 )
+(callback "set" get)
 
 
-(def getDecimalMatrix putDecimalMatrix)
+(def getDecimalMatrix (callback "getDecimalMatrix"))
 (if-not (= (count getDecimalMatrix) 2)
 	(throw (Exception. "getDecimalMatrix empty!"))
 )
+(callback "set" get)
 
-(def getDecimalMatrixAsList putDecimalMatrixAsList)
+(def getDecimalMatrixAsList (callback "getDecimalMatrixAsList"))
 (if-not (= (count getDecimalMatrixAsList) 2)
 	(throw (Exception. "getDecimalMatrixAsList empty!"))
 )
+(callback "set" get)
 
 
-(def getDoubleMatrix putDoubleMatrix)
+(def getDoubleMatrix (callback "getDoubleMatrix"))
 (if-not (= (count getDoubleMatrix) 2)
 	(throw (Exception. "getDoubleMatrix empty!"))
 )
+(callback "set" get)
 
-(def getDoubleMatrixAsList putDoubleMatrixAsList)
+(def getDoubleMatrixAsList (callback "getDoubleMatrixAsList"))
 (if-not (= (count getDoubleMatrixAsList) 2)
 	(throw (Exception. "getDoubleMatrixAsList empty!"))
 )
+(callback "set" get)
 
 
-(def getFloatMatrix putFloatMatrix)
+(def getFloatMatrix (callback "getFloatMatrix"))
 (if-not (= (count getFloatMatrix) 2)
 	(throw (Exception. "getFloatMatrix empty!"))
 )
+(callback "set" get)
 
-(def getFloatMatrixAsList putFloatMatrixAsList)
+(def getFloatMatrixAsList (callback "getFloatMatrixAsList"))
 (if-not (= (count getFloatMatrixAsList) 2)
 	(throw (Exception. "getFloatMatrixAsList empty!"))
 )
+(callback "set" get)
 
 
-(def getIntegerMatrix putIntegerMatrix)
+(def getIntegerMatrix (callback "getIntegerMatrix"))
 (if-not (= (count getIntegerMatrix) 2)
 	(throw (Exception. "getIntegerMatrix empty!"))
 )
+(callback "set" get)
 
-(def getIntegerMatrixAsList putIntegerMatrixAsList)
+(def getIntegerMatrixAsList (callback "getIntegerMatrixAsList"))
 (if-not (= (count getIntegerMatrixAsList) 2)
 	(throw (Exception. "getIntegerMatrixAsList empty!"))
 )
+(callback "set" get)
 
 
-(def getLongMatrix putLongMatrix)
+(def getLongMatrix (callback "getLongMatrix"))
 (if-not (= (count getLongMatrix) 2)
 	(throw (Exception. "getLongMatrix empty!"))
 )
+(callback "set" get)
 
-(def getLongMatrixAsList putLongMatrixAsList)
+(def getLongMatrixAsList (callback "getLongMatrixAsList"))
 (if-not (= (count getLongMatrixAsList) 2)
 	(throw (Exception. "getLongMatrixAsList empty!"))
 )
+(callback "set" get)
 
 
-(def getPercentMatrix putPercentMatrix)
+(def getPercentMatrix (callback "getPercentMatrix"))
 (if-not (= (count getPercentMatrix) 2)
 	(throw (Exception. "getPercentMatrix empty!"))
 )
+(callback "set" get)
 
-(def getPercentMatrixAsList putPercentMatrixAsList)
+(def getPercentMatrixAsList (callback "getPercentMatrixAsList"))
 (if-not (= (count getPercentMatrixAsList) 2)
 	(throw (Exception. "getPercentMatrixAsList empty!"))
 )
+(callback "set" get)
 
 
-(def getShortMatrix putShortMatrix)
+(def getShortMatrix (callback "getShortMatrix"))
 (if-not (= (count getShortMatrix) 2)
 	(throw (Exception. "getShortMatrix empty!"))
 )
+(callback "set" get)
 
-(def getShortMatrixAsList putShortMatrixAsList)
+(def getShortMatrixAsList (callback "getShortMatrixAsList"))
 (if-not (= (count getShortMatrixAsList) 2)
 	(throw (Exception. "getShortMatrixAsList empty!"))
 )
+(callback "set" get)
 
 
-(def getStringMatrix putStringMatrix)
+(def getStringMatrix (callback "getStringMatrix"))
 (if-not (= (count getStringMatrix) 2)
 	(throw (Exception. "getStringMatrix empty!"))
 )
+(callback "set" get)
 
-(def getStringMatrixAsList putStringMatrixAsList)
+(def getStringMatrixAsList (callback "getStringMatrixAsList"))
 (if-not (= (count getStringMatrixAsList) 2)
 	(throw (Exception. "getStringMatrixAsList empty!"))
 )
+(callback "set" get)
