34bcf6f361f0f7678d78ef5b570cfdb67a4fe2ec
subes <gsubes@gmail.com>
2023-11-25 20:08:53 +0000

    allow nan

--- invesdwin-context-parent/invesdwin-context-integration/src/main/java/de/invesdwin/context/integration/marshaller/MarshallerJsonJackson.java
+++ invesdwin-context-parent/invesdwin-context-integration/src/main/java/de/invesdwin/context/integration/marshaller/MarshallerJsonJackson.java
@@ -6,6 +6,7 @@ import javax.annotation.concurrent.ThreadSafe;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.json.JsonReadFeature;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -27,8 +28,6 @@ public final class MarshallerJsonJackson {
     private ObjectMapper newObjectMapper(final boolean multiline) {
         final ObjectMapper mapper = new ObjectMapper();
         configureObjectMapper(mapper, multiline);
-        //don't require quotes for field names
-        mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
         return mapper;
     }
 
@@ -41,6 +40,10 @@ public final class MarshallerJsonJackson {
         mapper.enable(SerializationFeature.WRITE_ENUMS_USING_TO_STRING);
         // Uses Enum.toString() for deserialization of an Enum
         mapper.enable(DeserializationFeature.READ_ENUMS_USING_TO_STRING);
+        //don't require quotes for field names
+        mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+        //allow nan without quotes
+        mapper.configure(JsonReadFeature.ALLOW_NON_NUMERIC_NUMBERS.mappedFeature(), true);
     }
 
     public static String toJson(final Object object, final boolean multiline) {
