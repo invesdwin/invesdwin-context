ae87bbb83fb25c45cb2d89d0aa669d15475e187b
Edwin Stang <gsubes@gmail.com>
2024-08-01 11:07:35 +0200

    PER-707 separate profit and loss colors for TradeProfile chart

---
+++ invesdwin-context-parent/invesdwin-context-jfreechart/src/main/java/de/invesdwin/context/jfreechart/util/LegendItems.java
@@ -0,0 +1,31 @@
+package de.invesdwin.context.jfreechart.util;
+
+import java.lang.reflect.Field;
+
+import javax.annotation.concurrent.Immutable;
+
+import org.jfree.chart.LegendItem;
+
+import de.invesdwin.util.lang.reflection.field.UnsafeField;
+
+@Immutable
+public final class LegendItems {
+
+    private static final UnsafeField<String> FIELD_LABEL;
+
+    static {
+        try {
+            final Field fieldLabel = LegendItem.class.getDeclaredField("label");
+            FIELD_LABEL = new UnsafeField<String>(fieldLabel);
+        } catch (final Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private LegendItems() {}
+
+    public static void setLabel(final LegendItem legendItem, final String label) {
+        FIELD_LABEL.put(legendItem, label);
+    }
+
+}
