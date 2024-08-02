d559b3f594d4b478c1356b13fbbd5f909653e8cd
Edwin Stang <gsubes@gmail.com>
2024-08-02 18:03:21 +0200

    PER-707 remove faulty heuristic to count toc pages and replace it with an actual page counting solution

--- invesdwin-context-parent/invesdwin-context-jasperreports/src/main/java/de/invesdwin/context/jasperreports/producer/PageLabelsClassicPdfProducer.java
+++ invesdwin-context-parent/invesdwin-context-jasperreports/src/main/java/de/invesdwin/context/jasperreports/producer/PageLabelsClassicPdfProducer.java
@@ -1,5 +1,7 @@
 package de.invesdwin.context.jasperreports.producer;
 
+import java.util.function.Supplier;
+
 import javax.annotation.concurrent.NotThreadSafe;
 
 import com.lowagie.text.pdf.PdfPageLabels;
@@ -11,27 +13,30 @@ import net.sf.jasperreports.export.pdf.classic.ClassicPdfProducer;
 @NotThreadSafe
 public class PageLabelsClassicPdfProducer extends ClassicPdfProducer {
 
-    private static final FastThreadLocal<PdfPageLabels> PAGE_LABELS_HOLDER = new FastThreadLocal<>();
+    private static final FastThreadLocal<Supplier<PdfPageLabels>> PAGE_LABELS_HOLDER = new FastThreadLocal<>();
 
-    private PdfPageLabels pageLabels = new PdfPageLabels();
+    private final Supplier<PdfPageLabels> pageLabelsSupplier;
 
     public PageLabelsClassicPdfProducer(final PdfProducerContext context) {
         super(context);
-        pageLabels = PAGE_LABELS_HOLDER.get();
+        pageLabelsSupplier = PAGE_LABELS_HOLDER.get();
         PAGE_LABELS_HOLDER.remove();
     }
 
     /**
      * Don't know if there is a better way to pass this information from the document builder to the producer.
      */
-    public static void setPageLabels(final PdfPageLabels pageLabels) {
+    public static void setPageLabelsSupplier(final Supplier<PdfPageLabels> pageLabels) {
         PAGE_LABELS_HOLDER.set(pageLabels);
     }
 
     @Override
     public void close() {
-        if (pageLabels != null) {
-            getPdfWriter().setPageLabels(pageLabels);
+        if (pageLabelsSupplier != null) {
+            final PdfPageLabels pageLabels = pageLabelsSupplier.get();
+            if (pageLabels != null) {
+                getPdfWriter().setPageLabels(pageLabels);
+            }
         }
         super.close();
     }
