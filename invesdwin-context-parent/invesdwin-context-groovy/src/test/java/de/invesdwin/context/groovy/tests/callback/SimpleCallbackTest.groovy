8a1d291a1e423b9dc7bb3515c4d74dc4902603b4
Edwin Stang <gsubes@gmail.com>
2024-10-12 13:54:14 +0200

    beanshell, groovy, javascript multiline expressions tests

--- invesdwin-context-parent/invesdwin-context-groovy/src/test/java/de/invesdwin/context/groovy/tests/callback/SimpleCallbackTest.groovy
+++ invesdwin-context-parent/invesdwin-context-groovy/src/test/java/de/invesdwin/context/groovy/tests/callback/SimpleCallbackTest.groovy
@@ -19,3 +19,26 @@ println("getSecretExpressionCallback")
 println(getSecretExpressionCallback)
 
 callback("voidMethod")
+
+double callManyParams = callback("callManyParams", true, 2, 3, '4', 5, 6, 7.0, 8.0, "123456789", 10.0)
+if(callManyParams != 55){
+	throw new Exception("callManyParams unexpected result: "+callManyParams)
+}
+double callManyParamsExpression = callback("callManyParamsExpression", true, 2, 3, '4', 5, 6, 7.0, 8.0, "123456789", 10.0)
+if(callManyParamsExpression != 55){
+	throw new Exception("callManyParamsExpression unexpected result: "+callManyParamsExpression)
+}
+double callManyParamsExpressionMultiline = callback("callManyParamsExpressionMultiline", true, 2, 3, '4', 5, 6, 7.0, 8.0, "123456789", 10.0)
+if(callManyParamsExpressionMultiline != 55){
+	throw new Exception("callManyParams unexpected result: "+callManyParamsExpressionMultiline)
+}
+
+getManyParamsExpression = putManyParamsExpression
+println("getManyParamsExpression")
+println(getManyParamsExpression)
+getManyParamsExpressionMultilineWrong = putManyParamsExpressionMultilineWrong
+println("getManyParamsExpressionMultilineWrong")
+println(getManyParamsExpressionMultilineWrong)
+getManyParamsExpressionMultiline = putManyParamsExpressionMultiline
+println("getManyParamsExpressionMultiline")
+println(getManyParamsExpressionMultiline)
--- invesdwin-context-parent/invesdwin-context-javascript/src/test/java/de/invesdwin/context/javascript/tests/callback/SimpleCallbackTest.js
+++ invesdwin-context-parent/invesdwin-context-javascript/src/test/java/de/invesdwin/context/javascript/tests/callback/SimpleCallbackTest.js
@@ -19,3 +19,26 @@ print("getSecretExpressionCallback")
 print(getSecretExpressionCallback)
 
 callback("voidMethod")
+
+callManyParams = callback("callManyParams", true, 2, 3, '4', 5, 6, 7.0, 8.0, "123456789", 10.0)
+if(callManyParams !== 55){
+	throw "callManyParams unexpected result: "+callManyParams
+}
+callManyParamsExpression = callback("callManyParamsExpression", true, 2, 3, '4', 5, 6, 7.0, 8.0, "123456789", 10.0)
+if(callManyParamsExpression !== 55){
+	throw "callManyParamsExpression unexpected result: "+callManyParamsExpression
+}
+callManyParamsExpressionMultiline = callback("callManyParamsExpressionMultiline", true, 2, 3, '4', 5, 6, 7.0, 8.0, "123456789", 10.0)
+if(callManyParamsExpressionMultiline !== 55){
+	throw "callManyParams unexpected result: "+callManyParamsExpressionMultiline
+}
+
+getManyParamsExpression = putManyParamsExpression
+print("getManyParamsExpression")
+print(getManyParamsExpression)
+getManyParamsExpressionMultilineWrong = putManyParamsExpressionMultilineWrong
+print("getManyParamsExpressionMultilineWrong")
+print(getManyParamsExpressionMultilineWrong)
+getManyParamsExpressionMultiline = putManyParamsExpressionMultiline
+print("getManyParamsExpressionMultiline")
+print(getManyParamsExpressionMultiline)
