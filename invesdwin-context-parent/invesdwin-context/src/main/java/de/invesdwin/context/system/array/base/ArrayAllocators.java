776ad821317bcef8bdef25fbc5a6a76d22921506
Edwin Stang <gsubes@gmail.com>
2026-07-02 18:39:18 +0200

    PER-1256 enable parallel loading in blackboard again

--- invesdwin-context-parent/invesdwin-context/src/main/java/de/invesdwin/context/system/array/base/ArrayAllocators.java
+++ invesdwin-context-parent/invesdwin-context/src/main/java/de/invesdwin/context/system/array/base/ArrayAllocators.java
@@ -2,7 +2,9 @@ package de.invesdwin.context.system.array.base;
 
 import javax.annotation.concurrent.Immutable;
 
-import de.invesdwin.util.concurrent.nested.DisabledNestedExecutor;
+import de.invesdwin.util.concurrent.Executors;
+import de.invesdwin.util.concurrent.WrappedExecutorService;
+import de.invesdwin.util.concurrent.nested.ANestedExecutor;
 import de.invesdwin.util.concurrent.nested.INestedExecutor;
 
 @Immutable
@@ -11,13 +13,13 @@ public final class ArrayAllocators {
     private ArrayAllocators() {}
 
     public static INestedExecutor newDefaultExecutor(final String name) {
-        return DisabledNestedExecutor.INSTANCE;
-        //        return new ANestedExecutor(name) {
-        //            @Override
-        //            protected WrappedExecutorService newNestedExecutor(final String nestedName) {
-        //                return Executors.newFixedCallerRunsThreadPool(nestedName, Executors.getCpuThreadPoolCount());
-        //            }
-        //        };
+        //        return DisabledNestedExecutor.INSTANCE;
+        return new ANestedExecutor(name) {
+            @Override
+            protected WrappedExecutorService newNestedExecutor(final String nestedName) {
+                return Executors.newFixedCallerRunsThreadPool(nestedName, Executors.getCpuThreadPoolCount());
+            }
+        };
     }
 
 }
