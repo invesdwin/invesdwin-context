1b999a5cc7ef7c68c977360b75aa1781d48bdc95
Edwin Stang <gsubes@gmail.com>
2026-09-11 18:56:34 +0200

    register shutdown hook for node slot lock without risking it being cleared during test inits

--- invesdwin-context-parent/invesdwin-context/src/main/java/de/invesdwin/context/beans/init/platform/DefaultPlatformInitializer.java
+++ invesdwin-context-parent/invesdwin-context/src/main/java/de/invesdwin/context/beans/init/platform/DefaultPlatformInitializer.java
@@ -47,8 +47,6 @@ import de.invesdwin.util.error.Throwables;
 import de.invesdwin.util.lang.Files;
 import de.invesdwin.util.lang.reflection.Reflections;
 import de.invesdwin.util.marshallers.serde.RemoteFastSerializingSerde;
-import de.invesdwin.util.shutdown.CloseableShutdownHook;
-import de.invesdwin.util.shutdown.ShutdownHookManager;
 import de.invesdwin.util.time.date.FDate;
 import de.invesdwin.util.time.date.FDates;
 import de.invesdwin.util.time.date.FTimeUnit;
@@ -301,12 +299,18 @@ public class DefaultPlatformInitializer implements IPlatformInitializer {
         while (true) {
             final File slotDir = new File(baseDir, "node_" + String.valueOf(node));
             final File lockFile = new File(slotDir, "process.lock");
+            //retain reference so that finalizer does not clean it during
             final HeartbeatFileChannelLock slotLock = new HeartbeatFileChannelLock(lockFile);
-
             if (slotLock.tryLock()) {
-                ShutdownHookManager.register(new CloseableShutdownHook(slotLock));
-                createDirectoryIfAllowed(slotDir);
-                return slotDir;
+                Runtime.getRuntime().addShutdownHook(new Thread() {
+                    @Override
+                    public void run() {
+                        slotLock.close();
+                    }
+                });
+                final File dataDir = new File(slotDir, "data");
+                createDirectoryIfAllowed(dataDir);
+                return dataDir;
             }
 
             node++;
